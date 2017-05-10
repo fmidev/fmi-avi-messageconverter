5224177a7677b36bfcb6e25d1b8799f20f728beb
Ilkka Rinne <ilkka.rinne@spatineo.com>
2017-05-10 16:13:29 +0300

    Added new appendAll method

--- src/main/java/fi/fmi/avi/parser/LexemeSequenceBuilder.java
+++ src/main/java/fi/fmi/avi/parser/LexemeSequenceBuilder.java
@@ -1,10 +1,14 @@
 package fi.fmi.avi.parser;
 
+import java.util.List;
+
 /**
  * Created by rinne on 01/03/17.
  */
 public interface LexemeSequenceBuilder {
     LexemeSequenceBuilder append(Lexeme lexeme);
+    
+    LexemeSequenceBuilder appendAll(List<Lexeme> lexemes);
 
     LexemeSequence build();
 }
