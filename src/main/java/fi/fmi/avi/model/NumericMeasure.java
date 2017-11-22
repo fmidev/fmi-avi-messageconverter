f41021848c25a58714462ed47ea05b1c994a7041
Ilkka Rinne <ilkka.rinne@spatineo.com>
2017-11-22 11:27:01 +0200

    Positive and negative zero values for temperature are now properly handled, fixes fmi-avi-messageconverter#10

--- src/main/java/fi/fmi/avi/model/NumericMeasure.java
+++ src/main/java/fi/fmi/avi/model/NumericMeasure.java
@@ -2,6 +2,14 @@ package fi.fmi.avi.model;
 
 public interface NumericMeasure extends AviationCodeListUser {
 
+    /**
+     * Returns the value, or null if value is unknown. Note that the value for zero
+     * may be either 0.0d or -0.0d, and the handling code may have to treat them differently.
+     * For testing it may be useful to know that 1.0d/0.0d == Double.POSITIVE_INFINITY and
+     * 1.0d/-0.0d == Double.NEGATIVE_INFINITY
+     *
+     * @return the provided measure value.
+     */
     Double getValue();
 
     void setValue(Double value);
