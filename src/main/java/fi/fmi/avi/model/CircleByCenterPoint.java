7a5e7faf0d904f7518847d4bd11de71ce3104ace
patrick-alaspaa <patrick.alaspaa@spatineo.com>
2020-05-06 10:44:34 +0300

    Add NumArc firld to circleByCenterPoint

--- src/main/java/fi/fmi/avi/model/CircleByCenterPoint.java
+++ src/main/java/fi/fmi/avi/model/CircleByCenterPoint.java
@@ -1,6 +1,8 @@
 package fi.fmi.avi.model;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Optional;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
@@ -9,4 +11,6 @@ public interface CircleByCenterPoint extends Geometry {
     List<Double> getCoordinates();
 
     NumericMeasure getRadius();
+
+    Optional<BigInteger> getNumarc();
 }
