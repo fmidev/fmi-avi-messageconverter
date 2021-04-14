81bf0076530fbbfaf3bcb72ad48c1e39114f48c0
Ernst de Vreede <ernst.de.vreede@knmi.nl>
2021-04-14 18:53:37 +0200

    Fixed test after merge with FMI

--- src/test/java/fi/fmi/avi/model/sigmet/immutable/SigmetTest.java
+++ src/test/java/fi/fmi/avi/model/sigmet/immutable/SigmetTest.java
@@ -1,11 +1,8 @@
 package fi.fmi.avi.model.sigmet.immutable;
 
 import static org.junit.Assert.assertEquals;
-<<<<<<< HEAD
-=======
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
->>>>>>> bc32561c4ce057ed63232c09ff54989739390daa
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -42,21 +39,11 @@ import fi.fmi.avi.model.sigmet.SigmetIntensityChange;
 
 public class SigmetTest {
 
-<<<<<<< HEAD
-    ObjectMapper om=new ObjectMapper().registerModule(new JavaTimeModule()).registerModule(new Jdk8Module())
-                .enable(SerializationFeature.INDENT_OUTPUT)
-                .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-
-    static String testGeoJson1 = "{\"type\":\"Polygon\",\"exteriorRingPositions\":[0,52,0,60,10,60,10,52,0,52]}}";
-
-    static String testGeoJson2 = "{\"type\":\"Polygon\",\"exteriorRingPositions\":[0,52,0,60,5,60,5,52,0,52]}}";
-=======
     private static final String TEST_GEO_JSON_1 = "{\"type\":\"Polygon\",\"exteriorRingPositions\":[0,52,0,60,10,60,10,52,0,52]}}";
     private static final String TEST_GEO_JSON_2 = "{\"type\":\"Polygon\",\"exteriorRingPositions\":[0,52,0,60,5,60,5,52,0,52]}}";
     private final ObjectMapper om = new ObjectMapper().registerModule(new JavaTimeModule())
             .registerModule(new Jdk8Module())
             .enable(SerializationFeature.INDENT_OUTPUT);
->>>>>>> bc32561c4ce057ed63232c09ff54989739390daa
 
     public PhenomenonGeometryWithHeight getAnalysis() {
         Optional<Geometry> anGeometry = Optional.empty();
@@ -67,7 +54,6 @@ public class SigmetTest {
             e.printStackTrace();
         }
 
-<<<<<<< HEAD
         TacOrGeoGeometryImpl.Builder builder = TacOrGeoGeometryImpl.builder();
         builder.setGeoGeometry(anGeometry.get());
         builder.setTacGeometry(TacGeometryImpl.builder().setData("ENTIRE FIR").build());
@@ -77,12 +63,6 @@ public class SigmetTest {
                 .setGeometry(builder.build())
                 .setApproximateLocation(false)
                 ;
-=======
-        final PhenomenonGeometryWithHeightImpl.Builder an = new PhenomenonGeometryWithHeightImpl.Builder().setTime(
-                PartialOrCompleteTimeInstant.of(ZonedDateTime.parse("2018-10-22T13:50:00Z")))
-                .setGeometry(TacOrGeoGeometryImpl.of(anGeometry.get()))
-                .setApproximateLocation(false);
->>>>>>> bc32561c4ce057ed63232c09ff54989739390daa
         return an.build();
     }
 
@@ -138,7 +118,6 @@ public class SigmetTest {
 
     @Test
     public void testBuild() throws IOException {
-<<<<<<< HEAD
         SIGMET sm=buildSigmet();
         assert(sm.areAllTimeReferencesComplete());
         System.err.println("TAC: "+sm.toString());
@@ -146,23 +125,11 @@ public class SigmetTest {
         System.err.println("JSON: "+json);
         JsonNode smNode=om.readTree(json.getBytes());
         assert(!smNode.isNull());
-        assert(smNode.has("status"));
-        assert(smNode.get("status").asText().equals("NORMAL"));
+        assert(smNode.has("reportStatus"));
+        assert(smNode.get("reportStatus").asText().equals("NORMAL"));
         assertEquals("ENTIRE FIR", smNode.get("analysisGeometries").get(0).get("geometry").get("tacGeometry").get("data").asText());
 
         SIGMET sm_reread = om.readValue(json, SIGMETImpl.class);
         System.err.println(">>>>"+sm_reread.getIssueTime());
-=======
-        final SIGMET sm = buildSigmet();
-        assertTrue(sm.areAllTimeReferencesComplete());
-        System.err.println("TAC: " + sm.toString());
-        final String json = om.writeValueAsString(sm);
-        System.err.println("JSON: " + json);
-        final JsonNode smNode = om.readTree(json.getBytes(StandardCharsets.UTF_8));
-        assertFalse(smNode.isNull());
-        assertTrue(smNode.has("reportStatus"));
-        assertEquals("NORMAL", smNode.get("reportStatus").asText());
-        assertTrue(!smNode.has("cancelMessage") || smNode.get("cancelMessage").asBoolean() == false);
->>>>>>> bc32561c4ce057ed63232c09ff54989739390daa
     }
 }
