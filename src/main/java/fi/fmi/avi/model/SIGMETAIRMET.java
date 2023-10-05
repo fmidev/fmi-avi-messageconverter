ef3ae5da35865364669024916485a67ce64f05fa
kasarit <49980037+kasarit@users.noreply.github.com>
2023-10-05 15:01:00 +0300

    Moved getAnalysisGeometries() to SIGMETAIRMET interface (#116)

--- src/main/java/fi/fmi/avi/model/SIGMETAIRMET.java
+++ src/main/java/fi/fmi/avi/model/SIGMETAIRMET.java
@@ -1,11 +1,16 @@
 package fi.fmi.avi.model;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SIGMETAIRMET extends AirTrafficServicesUnitWeatherMessage, AviationCodeListUser {
     String getSequenceNumber();
 
     PartialOrCompleteTimePeriod getValidityPeriod();
 
     Airspace getAirspace();
+
+    Optional<List<PhenomenonGeometryWithHeight>> getAnalysisGeometries();
 
     @Deprecated
     default SigmetAirmetReportStatus getStatus() {
