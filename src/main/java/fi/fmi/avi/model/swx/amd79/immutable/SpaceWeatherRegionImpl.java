b2fba3452c89a502404c8432415920e7eece45b2
kasarit <49980037+kasarit@users.noreply.github.com>
2025-10-29 16:53:05 +0200

    SDM-2682: Moved logic to model

--- src/main/java/fi/fmi/avi/model/swx/amd79/immutable/SpaceWeatherRegionImpl.java
+++ src/main/java/fi/fmi/avi/model/swx/amd79/immutable/SpaceWeatherRegionImpl.java
@@ -3,11 +3,16 @@ package fi.fmi.avi.model.swx.amd79.immutable;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import fi.fmi.avi.model.immutable.CircleByCenterPointImpl;
+import fi.fmi.avi.model.immutable.CoordinateReferenceSystemImpl;
+import fi.fmi.avi.model.immutable.NumericMeasureImpl;
 import fi.fmi.avi.model.swx.amd79.AirspaceVolume;
 import fi.fmi.avi.model.swx.amd79.SpaceWeatherRegion;
+import fi.fmi.avi.util.SubSolarPointUtils;
 import org.inferred.freebuilder.FreeBuilder;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -66,6 +71,37 @@ public abstract class SpaceWeatherRegionImpl implements SpaceWeatherRegion, Seri
                     .setLocationIndicator(value.getLocationIndicator().map(locationIndicator ->
                             SpaceWeatherLocation.valueOf(locationIndicator.name())));
         }
+
+        /**
+         * Sets the airspace volume's horizontal projection for
+         * {@link fi.fmi.avi.model.swx.amd79.SpaceWeatherRegion.SpaceWeatherLocation#DAYLIGHT_SIDE}.
+         * <p>
+         * If the airspace volume already has a geometry, returns the builder without modifications.
+         *
+         * @param analysisTime analysis' time
+         * @return builder instance
+         */
+        public Builder withComputedDaylightSideAirspaceVolume(final Instant analysisTime) {
+            getLocationIndicator()
+                    .filter(loc -> loc != SpaceWeatherLocation.DAYLIGHT_SIDE)
+                    .ifPresent(loc -> {
+                        throw new IllegalArgumentException("Location indicator is not DAYLIGHT SIDE: " + loc);
+                    });
+            setAirSpaceVolume(AirspaceVolumeImpl.builder()
+                    .setHorizontalProjection(
+                            CircleByCenterPointImpl.builder()
+                                    .setCrs(CoordinateReferenceSystemImpl.wgs84())
+                                    .setCenterPointCoordinates(
+                                            SubSolarPointUtils.computeSubSolarPoint(analysisTime))
+                                    .setRadius(NumericMeasureImpl.builder()
+                                            .setUom("km")
+                                            .setValue(SubSolarPointUtils.DAYLIGHT_SIDE_RADIUS_KM)
+                                            .build())
+                                    .build()
+                    ).build());
+            return this;
+        }
+
 
         @Override
         @JsonDeserialize(as = AirspaceVolumeImpl.class)
