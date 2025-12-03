f7ff603ed09ac05e4de44a8a6720af49e96175b3
Topi Kasari <49980037+kasarit@users.noreply.github.com>
2025-12-03 15:02:01 +0200

    SDM-2708: Nightside computation (#132)

--- src/test/java/fi/fmi/avi/util/SubSolarPointUtilsTest.java
+++ src/test/java/fi/fmi/avi/util/SubSolarPointUtilsTest.java
@@ -19,35 +19,46 @@ public class SubSolarPointUtilsTest {
     private final String timestamp;
     private final double expLat;
     private final double expLon;
+    private final double expAntiLat;
+    private final double expAntiLon;
 
-    public SubSolarPointUtilsTest(final String timestamp, final double expLat, final double expLon) {
+    public SubSolarPointUtilsTest(final String timestamp,
+                                  final double expLat,
+                                  final double expLon,
+                                  final double expAntiLat,
+                                  final double expAntiLon) {
         this.timestamp = timestamp;
         this.expLat = expLat;
         this.expLon = expLon;
+        this.expAntiLat = expAntiLat;
+        this.expAntiLon = expAntiLon;
     }
 
-    @Parameterized.Parameters(name = "{index}: {0} → lat={1} lon={2}")
+    @Parameterized.Parameters(name = "{index}: {0} → lat={1} lon={2} | anti={3},{4}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {"2016-11-08T00:06:00Z", -16.63, 174.44},
-                {"2016-11-08T01:00:00Z", -16.64, 160.94},
-                {"2016-11-08T07:00:00Z", -16.71, 70.94},
-                {"2016-11-08T13:00:00Z", -16.78, -19.05},
-                {"2016-11-08T19:00:00Z", -16.86, -109.05},
-                {"2016-11-09T01:00:00Z", -16.93, 160.96},
-                {"2020-11-08T01:00:00Z", -16.65, 160.94},
-                {"2025-10-27T06:00:00Z", -12.88, 85.97},
-                {"2025-10-27T12:00:00Z", -12.96, -4.04},
+                {"2016-11-08T00:06:00Z", -16.63, 174.44, 16.63, -5.56},
+                {"2016-11-08T01:00:00Z", -16.64, 160.94, 16.64, -19.06},
+                {"2016-11-08T07:00:00Z", -16.71, 70.94, 16.71, -109.06},
+                {"2016-11-08T13:00:00Z", -16.78, -19.05, 16.78, 160.95},
+                {"2016-11-08T19:00:00Z", -16.86, -109.05, 16.86, 70.95},
+                {"2016-11-09T01:00:00Z", -16.93, 160.96, 16.93, -19.04},
+                {"2020-11-08T01:00:00Z", -16.65, 160.94, 16.65, -19.06},
+                {"2025-10-27T06:00:00Z", -12.88, 85.97, 12.88, -94.03},
+                {"2025-10-27T12:00:00Z", -12.96, -4.04, 12.96, 175.96},
         });
     }
 
     @Test
     public void matches_expected_lat_lon() {
         final ZonedDateTime time = ZonedDateTime.parse(timestamp);
-        final List<Double> latLon = SubSolarPointUtils.computeSubSolarPoint(time.toInstant());
 
+        final List<Double> latLon = SubSolarPointUtils.computeSubSolarPoint(time.toInstant());
         assertEquals("latitude", expLat, latLon.get(0), TOLERANCE);
         assertEquals("longitude", expLon, latLon.get(1), TOLERANCE);
-    }
 
+        final List<Double> antiLatLon = SubSolarPointUtils.computeSubSolarPointAntipode(time.toInstant());
+        assertEquals("antipode latitude", expAntiLat, antiLatLon.get(0), TOLERANCE);
+        assertEquals("antipode longitude", expAntiLon, antiLatLon.get(1), TOLERANCE);
+    }
 }
