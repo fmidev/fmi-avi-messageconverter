49fea1669557d5dfa47481ad93fa4c8abe25d674
kasarit <49980037+kasarit@users.noreply.github.com>
2026-01-28 16:01:39 +0200

    SDM-2727: Cleanup

--- src/main/java/fi/fmi/avi/util/GTSExchangeFileInfo.java
+++ src/main/java/fi/fmi/avi/util/GTSExchangeFileInfo.java
@@ -330,7 +330,9 @@ public abstract class GTSExchangeFileInfo implements Serializable {
                     .setFileType(GTSExchangeFileType.fromExtension(m.group("type")))//
                     .setFreeFormPart(Optional.ofNullable(m.group("freeForm")))//
                     .setCompressionType(Optional.ofNullable(compressionType))//
-                    .setHeading(BulletinHeadingImpl.Builder.from(abbreviatedHeading).build())//
+                    .setHeading(BulletinHeadingImpl.Builder.from(abbreviatedHeading)
+                            .setOriginalCollectIdentifier(gtsExchangefileName)
+                            .build())//
                     .setTimeStampYear(timeStampYear)//
                     .setTimeStampMonth(timeStampMonth)//
                     .setTimeStampDay(timeStampDay)//
